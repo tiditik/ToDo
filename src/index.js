57f87a1482afe7bfb5008579b4087f02215cfd0e
Timur <timurlin212@gmail.com>
2025-03-25 14:23:40 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import figlet from "figlet";
+import Fastify from "fastify";
+import userRoute from "./users/userRoutes.js";
+
+const fastify = Fastify({
+  logger: true,
+});
+
+// Регистрация роутов.
+fastify.register(userRoute, { prefix: "api/user" });
+
+// Функция запуска сервера.
+const start = async () => {
+  try {
+    await fastify.listen({ port: Bun.env.PORT });
+    console.log(await figlet.text("TaskAlert"));
+    console.log(`Server is now listening on port ${Bun.env.PORT}`);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+// Запускаем сервер.
+start();
